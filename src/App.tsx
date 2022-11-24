b6ce05ea23c44532ccf88c05d089912f94eabd82
Kyle Silver <kyleAsilver@gmail.com>
2022-11-24 12:11:57 -0500

    it's rough, but it works

--- src/App.tsx
+++ src/App.tsx
@@ -15,9 +15,17 @@ const WheelRotationKeyframes = keyframes`
   100% { transform: rotate(360deg) }
 `;
 
-const WheelRotation = styled.div`
+const SecondsWheelRotation = styled.div`
   animation: ${WheelRotationKeyframes} 60s
     cubic-bezier(0.6, -0.28, 0.735, 0.045) infinite;
+`;
+
+const MinuteWheelRotation = styled.div`
+  animation: ${WheelRotationKeyframes} 3600s linear infinite;
+`;
+
+const HourWheelRotation = styled.div`
+  animation: ${WheelRotationKeyframes} calc(60 * 60 * 12) s linear infinite;
 `;
 
 const SecondsElement: React.FC<{ value: number; date: Date }> = ({
@@ -38,8 +46,7 @@ const SecondsElement: React.FC<{ value: number; date: Date }> = ({
       <div className="tick" />
     );
   return (
-    <div className="seconds-element" key={`seconds-${value}`} style={style}>
-      {/* {value.toFixed(0).padStart(2, "0")} */}
+    <div className="clock-element" key={`seconds-${value}`} style={style}>
       {element}
     </div>
   );
@@ -52,8 +59,81 @@ const SecondsWheel: React.FC<{ date: Date }> = ({ date }) => {
       return <SecondsElement value={index} date={date} />;
     });
   return (
-    <div className="seconds-wheel">
-      <WheelRotation>{elements}</WheelRotation>
+    <div className="clock-wheel">
+      <SecondsWheelRotation>{elements}</SecondsWheelRotation>
+    </div>
+  );
+};
+
+const MinutesElement: React.FC<{ value: number; date: Date }> = ({
+  value,
+  date,
+}) => {
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const time = minutes + seconds / 60;
+  const offset = time * (360 / 60);
+  const baseRotation = (60 - value) * (360 / 60);
+  const rotation = baseRotation + offset;
+  const style = {
+    transform: `rotate(${rotation}deg) translate(-125px)`,
+  };
+  const element =
+    value % 5 === 0 ? (
+      <div className="number">{value.toFixed(0).padStart(2, "0")}</div>
+    ) : (
+      <div className="tick" />
+    );
+  return (
+    <div className="clock-element" key={`seconds-${value}`} style={style}>
+      {element}
+    </div>
+  );
+};
+
+const MinuteWheel: React.FC<{ date: Date }> = ({ date }) => {
+  const elements = Array(60)
+    .fill(0)
+    .map((_, index) => {
+      return <MinutesElement value={index} date={date} />;
+    });
+  return (
+    <div className="minute-wheel">
+      <MinuteWheelRotation>{elements}</MinuteWheelRotation>
+    </div>
+  );
+};
+
+const HoursElement: React.FC<{ value: number; date: Date }> = ({
+  value,
+  date,
+}) => {
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const seconds = date.getSeconds();
+  const time = hours + minutes / 60 + seconds / 3600;
+  const offset = time * (360 / 60) * 5;
+  const baseRotation = (60 - value) * (360 / 60) * 5;
+  const rotation = baseRotation + offset;
+  const style = {
+    transform: `rotate(${rotation}deg) translate(-180px)`,
+  };
+  return (
+    <div className="clock-element" key={`seconds-${value}`} style={style}>
+      <div className="hour-number">{value.toFixed(0).padStart(2, "\xa0")}</div>
+    </div>
+  );
+};
+
+const HourWheel: React.FC<{ date: Date }> = ({ date }) => {
+  const elements = Array(12)
+    .fill(0)
+    .map((_, index) => {
+      return <HoursElement value={index + 1} date={date} />;
+    });
+  return (
+    <div className="hour-wheel">
+      <HourWheelRotation>{elements}</HourWheelRotation>
     </div>
   );
 };
@@ -63,6 +143,9 @@ function App() {
   return (
     <div className="clock">
       <SecondsWheel date={date} />
+      <MinuteWheel date={date} />
+      <HourWheel date={date} />
+      <div className="hand" />
     </div>
   );
 }
