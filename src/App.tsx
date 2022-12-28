9b4bdc2f274738003499dc56b7553ddd0198f6f4
Kyle Silver <kyleAsilver@gmail.com>
2022-12-28 17:55:45 -0500

    added router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import { Concentric } from "./Concentric/Concentric";
 import { Timescale } from "./Timescale/Timescale";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/timescale",
+    element: <Timescale />,
+  },
+  {
+    path: "/concentric",
+    element: <Concentric />,
+  },
+]);
 
 function App() {
-  // return <Timescale />;
-  return <Concentric />;
+  return <Timescale />;
+  // return <Concentric />;
+  // return <RouterProvider router={router} />;
 }
 
 export default App;
